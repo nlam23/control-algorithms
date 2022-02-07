63b7fca4b415077ea58d95826c048ffa324e4ce1
nlam23 <93894983+nlam23@users.noreply.github.com>
2022-02-06 19:28:30 -0800

    Update digital_in_out.py

--- circuitpython_examples/digital_in_out.py
+++ circuitpython_examples/digital_in_out.py
@@ -10,7 +10,9 @@ led.direction = Direction.OUTPUT
 
 # Switch setup
 switch = DigitalInOut(board.D2)
+#this is saying set a digital output on pin d2
 switch.direction = Direction.INPUT
+#this is saying that they're looking for an ionput.
 switch.pull = Pull.UP
 
 while True:
