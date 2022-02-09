68670ec3a2d014d80e05e885d807115b2be3c26c
nlam23 <93894983+nlam23@users.noreply.github.com>
2022-02-08 18:53:28 -0800

    Update analog_in.py

--- circuitpython_examples/analog_in.py
+++ circuitpython_examples/analog_in.py
@@ -5,6 +5,7 @@ from analogio import AnalogIn
 
 # Connect an analog sensor to the A1 port of the board you are using. This means a 3V, GND, and signal pin must all be connected.
 analog_in = AnalogIn(board.A1)
+#makes sure input is coming from pin A1
 
 # This function turns the analog reading of the sensor (a number from 0 - 65536) to a voltage.
 def get_voltage(pin):
