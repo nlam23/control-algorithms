39ce194738b0b1770d0295959019103949655400
nlam23 <93894983+nlam23@users.noreply.github.com>
2022-02-11 16:49:31 -0800

    Update cp_task_5.py

--- cp_light_control/cp_task_5.py
+++ cp_light_control/cp_task_5.py
@@ -2,8 +2,8 @@ import board
 from analogio import AnalogOut, AnalogIn
 
 led = AnalogOut(board.A0)
-light_sensor = AnalogIn(board.A1)
+lightSensor = AnalogIn(board.A1)
 led.value = 60000
 
 while True:
-  print(light_sensor.value)
+  print(lightSensor.value)
