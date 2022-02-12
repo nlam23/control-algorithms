c6367af5f27ad77276286516b46939310b0c6718
nlam23 <93894983+nlam23@users.noreply.github.com>
2022-02-11 16:41:59 -0800

    Update cp_task_2.py

--- cp_light_control/cp_task_2.py
+++ cp_light_control/cp_task_2.py
@@ -1,12 +1,11 @@
+import time
 import board
 from analogio import AnalogOut
 
-analog_out = AnalogOut(board.A0)
+led = AnalogOut(board.A0)
 
 while True:
 #counts up from 0 to 65535, with 64 increments, ends up corresponding to the DAC's 10-bit range
   for i in range(0, 65535, 64):
-    analog_out.value = i
-
-analog_out.value = 0
-  
+    led.value = i
+    time.sleep(0.1)
