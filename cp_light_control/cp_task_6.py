39875e8742b231ce1071a644331b830b4033b29c
nlam23 <93894983+nlam23@users.noreply.github.com>
2022-02-17 10:45:21 -0800

    Update cp_task_6.py

--- cp_light_control/cp_task_6.py
+++ cp_light_control/cp_task_6.py
@@ -1,2 +1,20 @@
+import board
+from analogio import AnalogOut, AnalogIn
+import time
 
+lightSensor = AnalogIn(board.A1)
+led = AnalogOut(board.A0)
+led.value = 0
+k = 0.1
+tolerance = 1000
+error = 0
+
+while True:
+  error = 30000 - lightSensor.value
+  if abs(error) < tolerance:
+    pass
+  else:
+    led.value -= error*k
     
+  print(led.value)
+  time.sleep(0.1)
